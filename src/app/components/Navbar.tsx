7c12525cc0ba32ed9c3a5016029ed26cbc8d966e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-09 04:58:38 +0100

    here
    I won't tolerate confilcts or failed anything

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+    return(
+        <div>
+            <ul className="flex">
+                <li>Peace</li>
+                <li>Peace</li>
+                <li>Peace</li>
+                <li>Peace</li>
+                <li>Peace</li>
+            </ul>
+        </div>
+    )
+}
