21543aec3596c0d3631af9f31bad5d19789464e8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-10 19:31:34 +0100

    lala

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,7 +3,7 @@ export default function Intro() {
         <div>
             I am Peace Oloruntoba, I am a programmer. <br />
             I went out to visit a family friend and I forgot my laptop charger and now my PC is about to die,<br />
-            I have deadlines to meet, but I won`&apos;`t let that stop me, Let`&apos;`s fire on<br />
+            I have deadlines to meet, but I won&apos;t let that stop me, Let&apos;s fire on<br />
         </div>
     )
 }
