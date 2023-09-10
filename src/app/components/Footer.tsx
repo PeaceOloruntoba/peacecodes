4c3e1a6c1094c01457ce99be32a50483054b7f0e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-10 19:18:46 +0100

    😔

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return(
         <div className="flex p-6">
+            <span>
             Footer
+            </span>
+            <span>
+                you can contact me on whatsapp: <a href="https://wa.me/+2348166846226">+234 816 684 6226</a><br />
+                or phone call: <a href="phone:+2349043101899"></a>
+            </span>
         </div>
     )
 }
---
+++ src/app/components/Intro.tsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+    return(
+        <div>
+            I am Peace Oloruntoba, I am a programmer. <br />
+            I went out to visit a family friend and I forgot my laptop charger and now my PC is about to die,<br />
+            I have deadlines to meet, but I won't let that stop me, Let's fire on<br />
+        </div>
+    )
+}
