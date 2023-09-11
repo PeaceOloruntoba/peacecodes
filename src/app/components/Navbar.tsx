682af9297ece93698408417f6293e9357b96c281
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-11 23:01:08 +0100

    nnn

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,26 +2,26 @@ import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <div className="flex items-center justify-center bg-slate-200 text-slate-800 font-bold">
+        <div className="flex items-center justify-around bg-slate-200 text-slate-800 font-bold">
             Logo
             <ul className="flex gap-5 p-5">
                 <li>
                 <Link href="/">Home</Link>
                 </li>
                 <li>
-                <Link href="/">About</Link>
+                <Link href="/About">About</Link>
                 </li>
                 <li>
-                <Link href="/">Experience</Link>
+                <Link href="/Experience">Experience</Link>
                 </li>
                 <li>
-                <Link href="/">Projects</Link>
+                <Link href="/Projects">Projects</Link>
                 </li>
                 <li>
-                <Link href="/">Blog</Link>
+                <Link href="/Blog">Blog</Link>
                 </li>
                 <li>
-                <Link href="/">Contact</Link>
+                <Link href="/Contact">Contact</Link>
                 </li>
             </ul>
             Theme-toggle
