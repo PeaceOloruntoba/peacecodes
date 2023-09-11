7c7309b356388368c524c82b95f72beadb9f029a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-11 18:05:12 +0100

    here

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,10 @@
 
 export default function Header() {
     return(
-        <div className="flex items-center justify-between px-14 py-2 bg-slate-300 dark:bg-slate-950">
-            <span>I am a open to connect and also to collaboration</span>
+        <div className="flex items-center justify-between px-14 py-2 bg-slate-300 darkbg-slate-950">
+            <span>
+                I am a open to connect and also to collaboration
+            </span>
             <span>
                 Social Icons Goes Here
             </span>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <div className="grid items-center justify-center">
+        <div className="grid items-center justify-center bg-slate-200 text-slate-800 font-bold">
             <ul className="flex gap-5 p-5">
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-                <a href="/">Home</a>
+                <li>
+                <Link href="/">Home</Link>
+                </li>
+                <Link href="/">About</Link>
+                <Link href="/">Experience</Link>
+                <Link href="/">Projects</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Contact</Link>
             </ul>
         </div>
     )
