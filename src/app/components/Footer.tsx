973d41748640b6b7f2b76431e9e4611eaaca9664
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-12 04:02:35 +0100

    here

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import Lastl from "./Lastl";
 
 export default function Footer() {
     return(
         <div className="grid">
             <div className="flex">
-            <span>
-
-            </span>
-            <span>
-
-            </span>
+                <ul className="flex items-center justify-center gap-10">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/">About</Link></li>
+                    <li><Link href="/">Skills</Link></li>
+                    <li><Link href="/">Projects</Link></li>
+                    <li><Link href="/">Blog</Link></li>
+                    <li><Link href="/">Contact</Link></li>
+                </ul>
             </div>
             <Lastl />
         </div>
