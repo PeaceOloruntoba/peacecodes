076dd38efa94a500c5cd24a7c80f0fb0133a158c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-13 01:56:47 +0100

    v

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,16 +3,16 @@ import me from "../assets/2021me.jpg"
 
 export default function Intro() {
     return(
-        <>
         <div className="flex justify-around items-center">
-            <div className="grid">
+            <div className="p-6 px-14 flex gap-5">
+            <div className="">
                 <h1>PEACE OLORUNTOBA</h1>
                 <h2>Full-Stack Software Engineer</h2>
             </div>
-            <div className="p-4 rounded-xl shadow-sm shadow-blue-900">
-                <Image src={me} alt="Jumbotron" className="rounded-xl" />
+            <div className="p-4 rounded-xl shadow-md shadow-blue-900">
+                <Image src={me} alt="Jumbotron" className="rounded-xl h-96 w-80" />
+            </div>
             </div>
         </div>
-        </>
     )
 }
