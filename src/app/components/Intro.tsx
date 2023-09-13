788a9201797b5edaafd71387651a27616bde87e5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-13 02:18:44 +0100

    a little replacement

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,8 +3,8 @@ import me from "../assets/2021me.jpg"
 
 export default function Intro() {
     return(
-        <div className="flex justify-around items-center px-36 dark:bg-gray-950 rounded-xl">
-            <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950">
+        <div className="flex justify-around items-center px-36 dark:bg-gray-950">
+            <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950 rounded-xl">
             <div className="">
                 <h1 className="text-4xl font-bold text-slate-800 dark:text-zinc-300">PEACE OLORUNTOBA</h1>
                 <h2 className="text-3xl font-semibold text-slate-800 dark:text-zinc-300 ">Full-Stack Software Engineer</h2>
