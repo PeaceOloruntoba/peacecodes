795448b0cb07239c24f166942147cc4822c088f7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-13 02:00:16 +0100

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     return(
         <div className="flex items-center justify-between px-14 py-2 bg-slate-300 dark:bg-gray-950 text-slate-900 dark:text-slate-200 font-semibold">
             <span>
-                I am a open to connect and also to collaboration
+                I am open to connect and also to collaborations
             </span>
             <span>
                 Social Icons Goes Here
