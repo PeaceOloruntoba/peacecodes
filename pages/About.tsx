703a207d287c80babaf19a03f71518bd3235fbed
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-15 03:19:37 +0100

    life is sweet 🍠

--- pages/About.tsx
+++ pages/About.tsx
@@ -3,6 +3,7 @@ import "../src/app/globals.css"
 import Navbar from "@/app/components/Navbar";
 
 import { Inter } from 'next/font/google'
+import AboutIntro from "./components/AboutIntro";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,9 +11,10 @@ export default function About() {
     return(
         <>
         <div className={inter.className}>
-        <div className="text-slate-800">
+        <div className="text-slate-800 dark:text-zinc-300 bg-slate-200 dark:bg-slate-950">
             <Navbar />
             <p className="text-5xl text-center">About</p>
+            <AboutIntro />
             <Footer />
         </div>
         </div>
--- pages/components/AboutIntro.tsx
+++ pages/components/AboutIntro.tsx
@@ -6,7 +6,6 @@ export default function AboutIntro() {
         <div className="flex justify-around items-center px-36 dark:bg-gray-950">
             <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950 rounded-xl">
             <div className="">
-                <span className="text-3xl font-semibold underline text-slate-800 dark:text-zinc-300">About Me:</span>
                 <h1 className="text-4xl font-bold text-slate-800 dark:text-zinc-300">PEACE OLORUNTOBA</h1>
                 <h2 className="text-3xl font-semibold text-slate-800 dark:text-zinc-300 ">Full-Stack Software Engineer</h2>
                 <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
