ea8facf7db1faa74423f38b08fca713faf34e6fa
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-19 03:48:11 +0100

    🤯

---
+++ src/app/components/HomeAbout.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import about from "../assets/homeabout.jpeg"
+
+export default function HomeAbout() {
+    return(
+        <div>
+        <div className="p-16 grid gap-5 items-center justify-center">
+            <div className="flex gap-5 items-center justify-center">
+            <Image src={about} alt="Jumbotron" className="rounded-xl" />
+            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">I am just me!</h2>
+            </div>
+        <div className="">
+            <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300 mb-8">I am a dedicated Computer Science student at Anchor University, Lagos, Nigeria, on a mission to transform my passion for technology into innovative solutions. As an aspiring Software Engineer, I've honed my skills over the past two years and am proficient in web development, while also venturing into the exciting world of mobile development... <a href="/About" className="text-purple-500">Learn More</a></p>
+        </div>
+            </div>
+            <div className="flex items-center justify-center px-16">
+            <hr className="w-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 h-1"/>
+            </div>
+    </div>
+    )
+}
--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,12 +1,13 @@
 
+import HomeAbout from "./HomeAbout";
 import Jumbotron from "./Jumbotron";
 
 export default function Intro() {
     return(
         <div className="flex justify-center items-center px-36 bg-slate-300 dark:bg-gray-950">
                 <div className="bg-slate-200 dark:bg-slate-950 rounded-xl ">
-            
             <Jumbotron />
+            <HomeAbout />
             </div>
         </div>
     )
