ccd62f46bbae98b0f25ae7d4e09a870bddc45376
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-19 03:24:24 +0100

    😡

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,19 +1,12 @@
-import Image from "next/image";
-import me from "../assets/2021me.jpg"
+
+import Jumbotron from "./Jumbotron";
 
 export default function Intro() {
     return(
-        <div className="flex justify-around items-center px-36 dark:bg-gray-950">
-            <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950 rounded-xl">
-            <div className="">
-                <span className="text-6xl font-bold text-slate-800 dark:text-zinc-300">Hi 👋, I'm</span>
-                <h1 className="text-5xl font-bold text-blue-600 mt-6 mb-2">Peace Oloruntoba</h1>
-                <h2 className="text-lg font-semibold text-zinc-600 mb-6">👨‍💻 Aspiring Software Engineer | 🌐 Web Developer | 📱 Mobile App Enthusiast | <br /> 💎 Blockchain Enthusiast | 🚀 Aspiring Innovator </h2>
-                <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div className="p-4 rounded-xl shadow-md shadow-blue-900">
-                <Image src={me} alt="Jumbotron" className="rounded-xl h-full w-full" />
-            </div>
+        <div className="flex justify-center items-center px-36 bg-slate-300 dark:bg-gray-950">
+                <div className="bg-slate-200 dark:bg-slate-950 rounded-xl ">
+            
+            <Jumbotron />
             </div>
         </div>
     )
---
+++ src/app/components/Jumbotron.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import me from "../assets/me.jpg"
+
+export default function Jumbotron() {
+    return(
+        <div>
+            <div className="p-16 flex gap-5 items-center">
+            <div className="">
+                <span className="text-6xl font-bold text-slate-800 dark:text-zinc-300">Hi 👋, I'm</span>
+                <h1 className="text-5xl font-bold text-blue-600 mt-8 mb-4">Peace Oloruntoba</h1>
+                <h2 className="text-lg font-semibold text-zinc-600 mb-8">👨‍💻 Aspiring Software Engineer | 🌐 Web Developer | 📱 Mobile App Enthusiast | <br /> 💎 Blockchain Enthusiast | 🚀 Aspiring Innovator </h2>
+                <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300 mb-8">I'm a dedicated Computer Science student at Anchor University, Lagos, with a two-year journey in web and mobile development. I thrive on challenges and relentlessly pursue knowledge in the ever-evolving IT landscape. Recently, I've ventured into blockchain development.</p>
+                <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300">I'm passionate about using technology to shape the future. Whether crafting web apps, diving into mobile development, or exploring blockchain's potential, I'm committed to creating meaningful solutions. Join me in this innovative journey, where we harness technology for a brighter future.</p>
+            </div>
+            <div className="p-4 rounded-xl shadow-md shadow-blue-900">
+                <Image src={me} alt="Jumbotron" className="rounded-xl" />
+            </div>
+                </div>
+                <div className="flex items-center justify-center px-16">
+                <hr className="w-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 h-1"/>
+                </div>
+        </div>
+    )
+}
