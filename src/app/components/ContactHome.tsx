91588e8cede269fe1181ee06b75932ea3268d7b3
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-23 19:24:02 +0100

    daa

--- src/app/components/ContactHome.tsx
+++ src/app/components/ContactHome.tsx
@@ -1,13 +1,17 @@
+import Image from "next/image";
+import about from "../../app/assets/aboutme.jpeg"
+
+
 export default function ContactHome(){
     return(
         <div className="grid items-center justify-center p-12">
             <h1 className="font-bold text-4xl text-slate-800 dark:text-zinc-300">Contact:</h1>
-            <div className="flex">
-                <div>
-
+            <div className="flex p-16 items-center justify-around rounded-xl shadow shadow-blue-600">
+                <div className="">
+                    <Image src={about} alt="Contact" />
                 </div>
                 <div>
-
+                    Social Icons
                 </div>
             </div>
         </div>
