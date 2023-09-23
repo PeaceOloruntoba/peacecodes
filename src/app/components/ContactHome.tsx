dbbbac2529583767054b7981925209ed6cc0f837
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-23 19:17:58 +0100

    🔥

--- src/app/components/ContactHome.tsx
+++ src/app/components/ContactHome.tsx
@@ -1,7 +1,15 @@
 export default function ContactHome(){
     return(
-        <div>
-            COntacttt
+        <div className="grid items-center justify-center p-12">
+            <h1 className="font-bold text-4xl text-slate-800 dark:text-zinc-300">Contact:</h1>
+            <div className="flex">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
