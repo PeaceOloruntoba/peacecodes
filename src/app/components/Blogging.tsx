cf9c068f071d90ed1b653cbe08329edf9f8e46f9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-23 19:04:49 +0100

    d

--- src/app/components/Blogging.tsx
+++ src/app/components/Blogging.tsx
@@ -8,12 +8,14 @@ export default function Blogging () {
         <div className="p-16 grid gap-5 items-center justify-center">
             <div className="flex gap-5 items-center justify-center">
             {/* <Image src={about} alt="Jumbotron" className="rounded-xl" /> */}
-            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Blogs:</h2>
+            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Articles:</h2>
             </div>
         <div className="shadow p-2 flex gap-5">
         <div className="border rounded-xl m-4">
         <Image src={about} alt="Jumbotron" className="rounded-t-xl w-full" />
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laudantium nostrum corrupti, odio esse excepturi delectus est. Ut voluptatum veritatis numquam. Est fugiat quae ex, atque minima sint consequatur commodi? <Link href="/" className="text-purple-500">Learn More...</Link></span>
+        <div className="p-5">
+        <span className="font-semibold text-slate-800 dark:text-zinc-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laudantium nostrum corrupti, odio esse excepturi delectus est. Ut voluptatum veritatis numquam. Est fugiat quae ex, atque minima sint consequatur commodi? <Link href="/" className="text-purple-500">Learn More...</Link></span>
+        </div>
         </div>
         <div className="border rounded-xl m-4">
         <Image src={about} alt="Jumbotron" className="rounded-t-xl w-full" />
