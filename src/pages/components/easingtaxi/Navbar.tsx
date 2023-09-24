17453a61386ec024ea6761593c291f538fe815c0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-24 13:19:37 +0100

    going nuts

--- src/pages/components/easingtaxi/Navbar.tsx
+++ src/pages/components/easingtaxi/Navbar.tsx
@@ -1,4 +1,18 @@
+import React, { useState } from 'react';
+
 export default function Navbar() {
+
+    
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsOpen(false);
+  };
+
     return(
         <div>
             Navbar
