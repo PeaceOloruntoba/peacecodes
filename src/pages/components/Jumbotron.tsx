470349a352b2d78f24646cdb0cb5d22f04977a7b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-25 01:37:43 +0100

    fff

--- src/pages/components/Jumbotron.tsx
+++ src/pages/components/Jumbotron.tsx
@@ -7,7 +7,7 @@ export default function Jumbotron() {
             <div>
             <Image src={leadd} alt="Godd" />
             <div>
-            <p className='text-gray-200'>Our IT section provides a variety of services, including website development, mobile app development, management system implementation, cyber security training, CCTV installation, and general IT consultation. We build customized solutions to match your particular demands using cutting-edge technology and experience, assuring flawless operations, increased security, and optimized business processes</p>
+            <p className='text-gray-200 font-semibold text-2xl'>Our IT section provides a variety of services, including website development, mobile app development, management system implementation, cyber security training, CCTV installation, and general IT consultation. We build customized solutions to match your particular demands using cutting-edge technology and experience, assuring flawless operations, increased security, and optimized business processes</p>
             </div>
             </div>
 
