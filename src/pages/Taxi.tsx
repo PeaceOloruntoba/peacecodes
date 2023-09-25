9e61ddff14b97acbe0e1e97a993457519f2655f3
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-25 01:37:14 +0100

    go 🌐

--- src/pages/Taxi.tsx
+++ src/pages/Taxi.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import "../app/globals.css"
+import Jumbotron from "./components/Jumbotron";
 
 export default function Taxi() {
     return(
@@ -7,6 +8,7 @@ export default function Taxi() {
         <div className=" bg-gradient-to-r from-blue-500 via-pink-500 to-red-500">
             <Navbar />
             Wanna see how thid looks
+            <Jumbotron />
         </div>
         </>
     )
