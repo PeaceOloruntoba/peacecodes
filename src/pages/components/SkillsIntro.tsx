908f961588d76f5798805b4bf0033539556e515c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 03:04:59 +0100

    add

--- src/pages/components/SkillsIntro.tsx
+++ src/pages/components/SkillsIntro.tsx
@@ -4,7 +4,8 @@ import me from "../../app/assets/2021me.jpg"
 export default function SkillsIntro() {
     return(
         <div className="flex justify-around items-center px-36 dark:bg-gray-950">
-            <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950 rounded-xl">
+            <div className="p-16 flex gap-5 bg-slate-200 dark:bg-slate-950 rounded-xl items-center justify-center">
+            <p className="text-5xl text-center px-4">Skill and Expertise:</p>
             <div className="">
                 <h1 className="text-4xl font-bold text-slate-800 dark:text-zinc-300">PEACE OLORUNTOBA</h1>
                 <h2 className="text-3xl font-semibold text-slate-800 dark:text-zinc-300 ">Full-Stack Software Engineer</h2>
