836d0cff19b75750168874080604887f358de415
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 15:44:30 +0100

    jingling

--- src/pages/components/Skills/Jumpon.tsx
+++ src/pages/components/Skills/Jumpon.tsx
@@ -1,7 +1,10 @@
 export default function Jumpon(){
     return(
         <div>
-            
+            <h1 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Skills and Expertise:</h1>
+            <div>
+                
+            </div>
         </div>
     )
 }
