e6854b33ba3d66e5a0d8708ec9e08558af244187
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 16:05:14 +0100

    dgfhfg

---
+++ src/pages/components/Skills/Lang.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface CardProps {
+  title: string;
+  content: string;
+  image: string;
+}
+
+const Lang: React.FC<CardProps> = ({ title, content, image }) => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+      <img src={image} alt={title} className="w-16 h-16 mx-auto rounded-full" />
+      <p className="text-2xl font-semibold mt-4">{title}</p>
+      <p className="text-gray-700 mt-2">{content}</p>
+    </div>
+  );
+};
+
+export default Lang;
