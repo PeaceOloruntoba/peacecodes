22fa328167a2988eb597c98935ecaeac19ca2944
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 15:34:46 +0100

    nbvcfghj

--- src/pages/components/SkillsIntro.tsx
+++ src/pages/components/SkillsIntro.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-import me from "../../app/assets/2021me.jpg"
+
+import Jumpon from "./Skills/Jumpon";
 
 export default function SkillsIntro() {
     return(
         <div className="flex justify-around items-center px-36 dark:bg-gray-950">
             <div className="p-16 bg-slate-200 dark:bg-slate-950 rounded-xl">
-                
+                <Jumpon />
             </div>
         </div>
     )
