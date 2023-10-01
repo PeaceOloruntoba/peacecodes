fdc217d37ee5cdef1e813864b64c4687e5a50484
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 16:01:59 +0100

    mnbbjh

--- src/pages/components/Skills/Jumpon.tsx
+++ src/pages/components/Skills/Jumpon.tsx
@@ -1,10 +1,32 @@
+
+const testimonialsData = [
+    {
+      author: "Micheal Gough",
+      company: "CEO at Google",
+      avatar: "/assets/me.jpg",
+    },
+    {
+      author: "Micheal Gough",
+      company: "CEO at Google",
+      avatar: "/assets/me.jpg",
+    },
+    {
+      author: "Micheal Gough",
+      company: "CEO at Google",
+      avatar: "/assets/me.jpg",
+    },
+    {
+      avatar: "/assets/me.jpg",
+      author: "Micheal Jordan",
+      company: "CEO at Microsoft",
+    },
+  ];
+
 export default function Jumpon(){
     return(
         <div>
             <h1 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Skills and Expertise:</h1>
-            <div>
-                
-            </div>
+            .
         </div>
     )
 }
