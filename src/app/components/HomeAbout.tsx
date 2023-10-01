0ca0d52c94addda4d99f81dc4735ceaa52e83f55
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 17:34:09 +0100

    HomeAbout Responsive

--- src/app/components/HomeAbout.tsx
+++ src/app/components/HomeAbout.tsx
@@ -6,9 +6,17 @@ export default function HomeAbout() {
     return(
         <div>
         <div className="p-16 grid gap-5 items-center justify-center">
+            <div className="hidden lg:block">
             <div className="flex gap-5 items-center justify-center">
             <Image src={about} alt="Jumbotron" className="rounded-xl" />
             <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">I am just me!</h2>
+            </div>
+            </div>
+            <div className="lg:hidden">
+            <div className="grid gap-6 items-center justify-center">
+            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">I am just me!</h2>
+            <Image src={about} alt="Jumbotron" className="rounded-xl" />
+            </div>
             </div>
         <div className="">
             <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300 mb-8">I am a Computer Science student at Anchor University, Lagos, Nigeria, on a mission to transform my passion for technology into innovative solutions. As an aspiring Software Engineer, I&apos;ve honed my skills over the past two years and am proficient in web development, while also venturing into the exciting world of mobile development... <Link href="/About" className="text-purple-500">Learn More</Link></p>
