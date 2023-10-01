fe233e4fb2bc2060c987e1f6a5dcf6362dab4eee
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-01 16:19:55 +0100

    tyuiyu

--- src/pages/components/Skills/Jumpon.tsx
+++ src/pages/components/Skills/Jumpon.tsx
@@ -1,24 +1,45 @@
+import Lang from "./Lang";
 
-const testimonialsData = [
+const cardData = [
     {
-      author: "Micheal Gough",
-      company: "CEO at Google",
-      avatar: "/assets/me.jpg",
+      title: "Micheal Gough",
+      content: "CEO at Google",
+      image: "/assets/me.jpg",
     },
     {
-      author: "Micheal Gough",
-      company: "CEO at Google",
-      avatar: "/assets/me.jpg",
+      title: "Micheal Gough",
+      content: "CEO at Google",
+      image: "/assets/me.jpg",
     },
     {
-      author: "Micheal Gough",
-      company: "CEO at Google",
-      avatar: "/assets/me.jpg",
+      title: "Micheal Gough",
+      content: "CEO at Google",
+      image: "/assets/me.jpg",
     },
     {
-      avatar: "/assets/me.jpg",
-      author: "Micheal Jordan",
-      company: "CEO at Microsoft",
+      image: "/assets/me.jpg",
+      title: "Micheal Jordan",
+      content: "CEO at Microsoft",
+    },
+    {
+      image: "/assets/me.jpg",
+      title: "Micheal Jordan",
+      content: "CEO at Microsoft",
+    },
+    {
+      image: "/assets/me.jpg",
+      title: "Micheal Jordan",
+      content: "CEO at Microsoft",
+    },
+    {
+      image: "/assets/me.jpg",
+      title: "Micheal Jordan",
+      content: "CEO at Microsoft",
+    },
+    {
+      image: "/assets/me.jpg",
+      title: "Micheal Jordan",
+      content: "CEO at Microsoft",
     },
   ];
 
@@ -26,7 +47,11 @@ export default function Jumpon(){
     return(
         <div>
             <h1 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Skills and Expertise:</h1>
-            .
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {cardData.map((card, index) => (
+            <Lang key={index} title={card.title} content={card.content} image={card.image} />
+            ))}
+            </div>
         </div>
     )
 }
--- src/pages/components/Skills/Lang.tsx
+++ src/pages/components/Skills/Lang.tsx
@@ -8,9 +8,9 @@ interface CardProps {
 
 const Lang: React.FC<CardProps> = ({ title, content, image }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+    <div className="p-6 rounded-lg shadow-lg text-center group">
       <img src={image} alt={title} className="w-16 h-16 mx-auto rounded-full" />
-      <p className="text-2xl font-semibold mt-4">{title}</p>
+      <p className="text-2xl font-semibold mt-4 ">{title}</p>
       <p className="text-gray-700 mt-2">{content}</p>
     </div>
   );
