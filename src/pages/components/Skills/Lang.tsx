5bd85046c8cce9236ade9e0a1be6ff98a5b459a7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-03 12:30:57 +0100

    djhdfj

--- src/pages/components/Skills/Lang.tsx
+++ src/pages/components/Skills/Lang.tsx
@@ -10,8 +10,8 @@ const Lang: React.FC<CardProps> = ({ title, content, image }) => {
   return (
     <div className="p-6 rounded-lg shadow-lg text-center group">
       <img src={image} alt={title} className="w-16 h-16 mx-auto rounded-full" />
-      <p className="text-2xl font-semibold mt-4 ">{title}</p>
-      <p className="text-gray-500 mt-2">{content}</p>
+      <p className="text-2xl font-semibold mt-4 text-slate-800 dark:text-zinc-300">{title}</p>
+      <p className="text-gray-600 font-semibold mt-2">{content}</p>
     </div>
   );
 };
