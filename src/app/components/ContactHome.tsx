3745a9c4b7f1958a79f8acc57e98675d9d3b42a5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-05 13:55:15 +0100

    dfcgv

--- src/app/components/ContactHome.tsx
+++ src/app/components/ContactHome.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import about from "../../app/assets/aboutme.jpeg"
 import Link from "next/link";
-import {RiTwitterXFill} from "react-icons/ri"
+import { RiTwitterXFill, RiGithubFill, RiLinkedinBoxFill } from "react-icons/ri"
 
 
 export default function ContactHome(){
@@ -12,9 +12,13 @@ export default function ContactHome(){
                 <div className="">
                     <Image src={about} alt="Contact" />
                 </div>
-                <div>
-                    <span className="">
-                        <RiTwitterXFill />
+                <div className="grid items-center justify-center p-12">
+                    <span className="text-slate-800 dark:text-zinc-300 py-2 text-2xl font-semibold flex items-center justify-around">
+                        <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiTwitterXFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
+                        <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiGithubFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
+                        <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiLinkedinBoxFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
+                        <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiTwitterXFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
+
                     </span>
                     <Link href="/Contact"><button className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white font-semibold p-4 text-xl px-10 rounded-lg">Contact Me</button></Link>
                 </div>
