0b81a0c727a00c3a0d05ca9f1613c9452dca01d6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-05 13:28:31 +0100

    should be alright

--- src/pages/components/Skills/Lang.tsx
+++ src/pages/components/Skills/Lang.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface CardProps {
@@ -9,7 +10,7 @@ interface CardProps {
 const Lang: React.FC<CardProps> = ({ title, content, image }) => {
   return (
     <div className="rounded-lg shadow-lg text-center group">
-      <img src={image} alt={title} className="w-full h-32 mx-auto rounded-t-lg group-hover:scale-105 ease-in-out duration-300" />
+      <Image src={image} alt={title} className="w-full h-32 mx-auto rounded-t-lg group-hover:scale-105 ease-in-out duration-300" />
       <div className='pb-4 px-2'>
       <p className="text-xl font-semibold mt-4 text-slate-800 dark:text-zinc-300 group-hover:text-2xl ease-in-out duration-300">{title}</p>
       <p className="text-gray-600 mt-2 group-hover:text-lg ease-in-out duration-300">{content}</p>
