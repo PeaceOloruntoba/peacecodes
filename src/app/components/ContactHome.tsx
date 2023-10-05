e256d8589ecc1051dcd5c4f0485722ff7e3ffb3b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-05 14:12:48 +0100

    fghjfs

--- src/app/components/ContactHome.tsx
+++ src/app/components/ContactHome.tsx
@@ -8,9 +8,9 @@ export default function ContactHome(){
     return(
         <div className="grid items-center justify-center p-10">
             <h1 className="font-bold text-4xl text-slate-800 dark:text-zinc-300">Contact:</h1>
-            <div className="flex p-16 items-center justify-around rounded-xl shadow shadow-blue-600">
-                <div className="grid items-center justify-center p-12">
-                    <p>You can reach me through the following means</p>
+            <div className="px-16 rounded-xl shadow shadow-blue-600 mt-4">
+                <div className="grid items-center justify-center p-12 px-16">
+                    <p className="text-slate-800 dark:text-zinc-300 py-3 text-xl font-semibold">You can reach me through the following means:</p>
                     <span className="text-slate-800 dark:text-zinc-300 py-2 text-2xl font-semibold flex items-center justify-around">
                         <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiTwitterXFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
                         <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiWhatsappFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
@@ -18,8 +18,10 @@ export default function ContactHome(){
                         <Link href="https://twitter.com/PeaceEdgeTech" target="_blank"><RiFacebookBoxFill className="hover:text-3xl cursor-pointer ease-in-out duration-300" /></Link>
 
                     </span>
+                    <div className="grid items-center justify-center py-10">
                     <Link href="/Contact"><button className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white font-semibold p-4 text-xl px-10 rounded-lg">Contact Me</button></Link>
-                    <p>Let&apos;s discuss business!!</p>
+                    <p className="text-slate-600 dark:text-zinc-500 py-3 text-lg">Let&apos;s discuss business!!</p>
+                    </div>
                 </div>
             </div>
         </div>
