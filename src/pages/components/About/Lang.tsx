2f93aa6913c2837a0513b4cdc0b0babc7143283d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-12 08:44:03 +0100

    dsfds

--- src/pages/components/About/Lang.tsx
+++ src/pages/components/About/Lang.tsx
@@ -1,7 +1,12 @@
 export default function(){
     return(
         <div>
-            
+            <div>
+                <p className="text-3xl font-semibold">Experience:</p>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
