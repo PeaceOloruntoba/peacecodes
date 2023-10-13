a2cea45f3f069794bce1059df52f9b84af1915e3
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-13 09:11:46 +0100

    hgjhkjkj

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 
 export default function Header() {
-    return(
+    return (
         <div className="flex items-center justify-between px-4 lg:px-14 py-2 bg-slate-300 dark:bg-gray-950 text-slate-900 dark:text-slate-200 font-semibold">
             <span className="hidden md:block lg:block">
                 I am open to connect and also to collaborations
