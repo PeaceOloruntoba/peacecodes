d18db54dd46019af8bb49d468d41b3bfbd84ef1a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-13 09:12:44 +0100

    hgfhj

--- src/app/components/Lastl.tsx
+++ src/app/components/Lastl.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Lastl() {
-    return(
+    return (
         <div className="flex items-center justify-between py-2 pt-6">
             <span>Copyright © 2023 <Link href="/" className="text-blue-600">Peace Oloruntoba</Link></span>
             <span className="hidden lg:block">Social icons goes here</span>
