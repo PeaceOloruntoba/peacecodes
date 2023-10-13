d2709a5a9144f893b445c0e14ff8704cacd89c5e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-13 08:39:00 +0100

    hghjk

--- src/pages/components/Contact/Jumpon.tsx
+++ src/pages/components/Contact/Jumpon.tsx
@@ -9,13 +9,44 @@ export default function Jumpon(){
             <div className="bg-slate-200 dark:bg-slate-950 rounded-xl">
            <div className="p-16 px-10 grid gap-5 items-center justify-center">
             <div className="grid gap-5 items-center justify-center text-center">
-            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">About me:</h2>
-            <Image src={about} alt="Jumbotron" className="rounded-xl w-96" />
+            <h2 className="text-4xl font-semibold text-slate-800 dark:text-zinc-300">Contact me:</h2>
             </div>
-        <div className="">
-            <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300 mb-8">I am <span className="text-blue-500 font-bold">Peace Oloruntoba</span>, a dedicated Computer Science student at <Link href="https://aul.edu.ng/" className="text-purple-500">Anchor University, Lagos, Nigeria</Link>, on a mission to transform my passion for technology into innovative solutions. As an aspiring Software Engineer, I&apos;ve honed my skills over the past two years and am proficient in web development, while also venturing into the exciting world of mobile development. My journey in the field of software development has been marked by a relentless pursuit of knowledge and an unwavering commitment to pushing the boundaries of what&apos;s possible in the realm of technology.</p>
-            <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300 mb-8">My enthusiasm for coding goes beyond the classroom, as I am a passionate developer who thrives on the challenges presented by the ever-evolving IT landscape. I have an insatiable curiosity that drives me to continuously explore new technologies, making me well-versed in the latest industry trends. This curiosity has led me to dive into blockchain development, a fascinating and cutting-edge field that I&apos;ve been actively exploring for the past few weeks.</p>
-            <p className="text-xl font-semibold text-slate-800 dark:text-zinc-300">I firmly believe that technology has the power to shape the future, and I am determined to be at the forefront of this transformative journey. Whether it&apos;s crafting elegant web applications, delving into the intricacies of mobile app development, or exploring the decentralized world of blockchain, I am committed to creating meaningful solutions that improve the lives of individuals and businesses alike.</p>
+        <div className="flex">
+        <div className="p-2 grid">
+            <Image src={about} alt="Jumbotron" className="rounded-xl w-96" />
+            <p>Peace Oloruntoba</p>
+            <p>Full-Stack Web Developer / Aspiring Software Engineer</p>
+            <p>Please fill the form to send me an official mail,<br />and anticipate a response within 24hrs</p>
+        </div>
+        <div className=" p-2">
+          <form action="" className="text-slate-800 dark:text-zinc-300 font-semibold">
+            <div className="flex">
+              <div className="grid p-2">
+            <label htmlFor="">Name:</label>
+            <input type="text" className="bg-transparent border rounded-xl p-2" />
+              </div>
+              <div className="grid p-2">
+            <label htmlFor="">Phone Number:</label>
+            <input type="tel" className="bg-transparent border rounded-xl p-2" />
+              </div>
+            </div>
+              <div className="grid p-2">
+            <label htmlFor="">Email:</label>
+            <input type="email" className="bg-transparent border rounded-xl p-2" />
+              </div>
+              <div className="grid p-2">
+            <label htmlFor="">Subject:</label>
+            <input type="text" className="bg-transparent border rounded-xl p-2" />
+              </div>
+              <div className="grid p-2">
+            <label htmlFor="">Message:</label>
+            <textarea name="" id="" cols="30" rows="10" className="bg-transparent border rounded-xl p-2">Hi,</textarea>
+              </div>
+              <div className="p-2">
+                <button type="submit" className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-blue-500 p-2 w-full rounded-xl font-semibold text-lg ease-in-out duration-300">Send Message</button>
+              </div>
+          </form>
+        </div>
         </div>
             </div>
             <div className="flex items-center justify-center px-16">
