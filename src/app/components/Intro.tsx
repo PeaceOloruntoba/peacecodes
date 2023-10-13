26d9d52e405f30e40416e7005075f9d735baec1e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-13 09:12:16 +0100

    ghjkjh

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -5,13 +5,13 @@ import HomeAbout from "./HomeAbout";
 import Jumbotron from "./Jumbotron";
 
 export default function Intro() {
-    return(
+    return (
         <div className="flex justify-center items-center lg:px-20 md:px-12 sm:px-6 bg-slate-300 dark:bg-gray-950">
             <div className="bg-slate-200 dark:bg-slate-950 rounded-xl">
-            <Jumbotron />
-            <HomeAbout />
-            <Blogging />
-            <ContactHome />
+                <Jumbotron />
+                <HomeAbout />
+                <Blogging />
+                <ContactHome />
             </div>
         </div>
     )
