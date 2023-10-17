6312cc0eaf6e37487dee97dd96f2902260b72d43
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-17 15:20:27 +0100

    kjhgfd

--- src/pages/components/About/Lang.tsx
+++ src/pages/components/About/Lang.tsx
@@ -4,7 +4,7 @@ export default function(){
             <div>
                 <p className="text-3xl font-semibold">Experience:</p>
                 <div>
-
+                    I have a sound 2 years of experience in web development, in my 3rd month of front-end development, I ventured into backend development, and within 6 months, I became an expert full-stack web developer,
                 </div>
             </div>
         </div>
