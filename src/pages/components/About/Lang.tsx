97c34329861200d07b7dbbae1614f3be45d44252
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-17 15:24:45 +0100

    rest

--- src/pages/components/About/Lang.tsx
+++ src/pages/components/About/Lang.tsx
@@ -4,8 +4,7 @@ export default function(){
             <div>
                 <p className="text-3xl font-semibold">Experience:</p>
                 <div>
-                    I have a sound 2 years of experience in web development, in my 3rd month of front-end development, I ventured into backend development, and within 6 months, I became an expert full-stack web developer,
-                    over time, I have worked as a contractual web developer before I decided to up my game in software engineering with over a year training as a software developer, I have been able to refine my skills as a full-stack developer, and engineer, venturing in web, mobile and software development. I've worked with a number of teams of developers, as a front-end engineer, or a backend engineer, also as a full-stack engineer, and also right now, I even take on mentees which I mentor as front-end, or back-end, or full-stack developer
+                    I have a sound 2 years of experience in web development, in my 3rd month of front-end development, I ventured into backend development, and within 6 months, I became an expert full-stack web developer, over time, I have worked as a contractual web developer before I decided to up my game in software engineering with over a year training as a software developer, I have been able to refine my skills as a full-stack developer, and engineer, venturing in web, mobile and software development. I've worked with a number of teams of developers, as a front-end engineer, or a backend engineer, also as a full-stack engineer, and also right now, I even take on mentees which I mentor as front-end, or back-end, or full-stack developer
                 </div>
             </div>
         </div>
