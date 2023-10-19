fc7e2883711a82150785a9450ab982e072f1fb9f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-20 00:54:57 +0100

    bvnm,

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,9 @@
 import "../app/globals.css"
-import Footer from "@/app/components/Footer";
-import Navbar from "@/app/components/Navbar";
-
 import { Inter } from 'next/font/google'
-import AboutIntro from "./components/AboutIntro";
 import Head from "next/head";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import AboutIntro from "./components/AboutIntro";
 
 const inter = Inter({ subsets: ['latin'] })
 
