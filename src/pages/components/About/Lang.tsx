cd4b0541468db57800f6c42204d6710ba9fa8d48
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-11-06 11:10:09 +0100

    vbn

--- src/pages/components/About/Lang.tsx
+++ src/pages/components/About/Lang.tsx
@@ -1,11 +1,45 @@
-export default function(){
-    return(
+;
+export default function () {
+    return (
         <div>
             <div>
                 <p className="text-3xl font-semibold">Experience:</p>
                 <div>
                     I have a sound 2 years of experience in web development, in my 3rd month of front-end development, I ventured into backend development, and within 6 months, I became an expert full-stack web developer, over time, I have worked as a contractual web developer before I decided to up my game in software engineering with over a year training as a software developer, I have been able to refine my skills as a full-stack developer, and engineer, venturing in web, mobile and software development. I've worked with a number of teams of developers, as a front-end engineer, or a backend engineer, also as a full-stack engineer, and also right now, I even take on mentees which I mentor as front-end, or back-end, or full-stack developer
                 </div>
+                <ol className="relative border-l border-gray-200 dark:border-gray-700 mt-14">
+                    <li className="mb-10 ml-6">
+                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                            </svg>
+                        </span>
+                        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Full-Stack Developer Easing Life Groups of Companies<span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Currently</span></h3>
+                        <time className="block mb-2 text-sm text-gray-400 dark:text-gray-500">December, 2022</time>
+                        <p className="mb-4 text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+                    </li>
+                    <li className="mb-10 ml-6">
+                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                            </svg>
+                        </span>
+                        <h3 className="mb-1 text-xl font-semibold text-slate-800 dark:text-zinc-300">Concly Dev, Germany<span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Currently</span></h3>
+                        <time className="block mb-2 text-sm text-gray-400 dark:text-gray-500">May, 2023</time>
+                        <p className="font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+                    </li>
+                    <li className="ml-6">
+                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                            </svg>
+                        </span>
+                        <h3 className="mb-1 text-xl font-semibold text-slate-800 dark:text-zinc-300">Android Dev Intern</h3>
+                        <time className="block mb-2 text-sm text-gray-400 dark:text-gray-500">July 2023 - September 2023</time>
+                        <p className="font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+                    </li>
+                </ol>
+
             </div>
         </div>
     )
