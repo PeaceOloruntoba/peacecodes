728fbe0b9f90f140648e5f162a3c5158caac4dbe
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-01-03 12:43:34 +0100

    go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Peace Oloruntoba - Software Engineer',
-  description: 'NextTS and TailwindCSS',
+  description: 'Peace Oloruntoba is a software developer, and an aspiring Software Engineer, with over 2 years of experience in web, and mobile development, with over 10 web projects, and over 2 mobile apps completed, Peace has honed his special skills that makes him unique',
 }
 
 export default function RootLayout({
