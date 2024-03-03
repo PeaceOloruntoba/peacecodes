d6fac97d09d7978258f815108c686fb123ba9be4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-03 21:39:02 +0100

    asdf

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,6 +15,59 @@ export default function Navbar() {
   const closeSidebar = () => {
     setIsOpen(false);
   };
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
+  )
+  const element = document.documentElement
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+  console.log(darkQuery,"darkQuery")
+  const options = [
+    {
+      icon: <IoSunnyOutline />, 
+      text: "light"
+    },
+    {
+      icon: <IoMoon />, 
+      text: "dark"
+    },
+    {
+      icon: <IoDesktopOutline />, 
+      text: "system"
+    }
+  ]
+  function onWindowMatch(){
+    if(localStorage.theme === 'dark' || (!("theme" in localStorage) && darkQuery.matches)){
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark")
+    }
+  }
+  onWindowMatch()
+  useEffect(()=>{
+    switch (theme) {
+      case 'dark':
+        element.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      case 'light':
+        element.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        break;
+      default:
+        localStorage.removeItem("theme");
+        onWindowMatch()
+        break;
+    }
+  },[theme])
+  darkQuery.addEventListener("change", (e) => {
+    if (!("theme" in localStorage)){
+      if (e.matches){
+        element.classList.add("dark")
+      } else {
+        element.classList.remove("dark")
+      }
+    }
+  })
   return (
     <div>
       <Header />
